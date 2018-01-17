fc8814d021b1b324239d798b5a520bd8138f5e47
Lucasalk <35541227+Lucasalk@users.noreply.github.com>
2018-01-17 18:23:32 -0200

    Update and rename wda_crud.sql to projeto.sql

---
+++ projeto.sql
@@ -0,0 +1,6 @@
+Database: Projeto
+CREATE TABLE IF NOT EXISTS `Cadastro` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
